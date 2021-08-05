0d331120a206f60bbf4a3d38dd8e4a7f6d550e30
nazarpunk <nazarpunk@gmail.com>
2021-08-06 02:25:29 +0300

    temp

--- extension/js/content.ts
+++ extension/js/content.ts
@@ -8,35 +8,35 @@
 					       node.text.indexOf(`$.widget.bridge`) >= 0 ||
 					       node.text.indexOf(`function get_name_browser`) >= 0
 				       ) ? node.remove() : false;
-			})
-		})
+			});
+		});
 	}).observe(document, {
 		childList: true,
 		subtree  : true
 	});
-	
+
 	const favicon = (path: string) => {
 		let favicon = document.querySelector<HTMLLinkElement>('link[rel="shortcut icon"]');
 		if (!favicon) {
-			favicon     = document.createElement('link');
+			favicon = document.createElement('link');
 			favicon.rel = 'shortcut icon';
-			let $head   = document.querySelector<HTMLHeadElement>('head');
+			let $head = document.querySelector<HTMLHeadElement>('head');
 			if ($head !== null) $head.appendChild(favicon);
 		}
 		favicon.type = 'image/png';
 		favicon.href = path;
-	}
-	
+	};
+
 	const js = (path: string) => {
-		let $js   = document.createElement(`script`);
-		$js.src   = `chrome-extension://${chrome.runtime.id}/${path}`;
+		let $js = document.createElement(`script`);
+		$js.src = `chrome-extension://${chrome.runtime.id}/${path}`;
 		$js.async = false;
 		document.head.appendChild($js);
-	}
-	
+	};
+
 	window.addEventListener('DOMContentLoaded', () => {
 		document.head.setAttribute(`data-extension-id`, chrome.runtime.id);
-		
+
 		favicon(`chrome-extension://${chrome.runtime.id}/img/ico/ext-16.png`);
 		js(`page/page.js`);
 	});
